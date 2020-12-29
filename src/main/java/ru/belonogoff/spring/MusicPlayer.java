61d653e06222cf99fb99e1047a468197459c0110
Dmitriy Belonogov <belonogoff90@gmail.com>
2020-12-29 16:38:49 +0300

    8 step

--- src/main/java/ru/belonogoff/spring/MusicPlayer.java
+++ src/main/java/ru/belonogoff/spring/MusicPlayer.java
@@ -1,57 +1,22 @@
 package ru.belonogoff.spring;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class MusicPlayer {
 
-    private List<Music> playList = new ArrayList<>();
-    private String name;
-    private int volume;
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    public MusicPlayer(List<Music> playList) {
-        this.playList = playList;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public MusicPlayer() {
-    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println(music.getSong());
-    }
-
-    public void playMusicList() {
-        for (Music song : playList) {
-            System.out.println("Playing " + song.getSong());
-        }
-    }
-
-    public List<Music> getPlayList() {
-        return playList;
-    }
-
-    public void setPlayList(List<Music> playList) {
-        this.playList = playList;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 }
--- src/main/java/ru/belonogoff/spring/TestSpring.java
+++ src/main/java/ru/belonogoff/spring/TestSpring.java
@@ -7,13 +7,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music rockMusic = context.getBean("someRockMusic", Music.class);
-        MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
-        rockMusicPlayer.playMusic();
-
-        Music classicalMusic = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicalMusic);
-        classicalMusicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
